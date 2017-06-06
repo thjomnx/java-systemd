5f9d9c3d6695c93fbf09b00a80521ad03f6d57f0
Markus Enax <Markus.Enax@gmail.com>
2017-06-06 14:09:53 +0200

    Add more unit tests

---
+++ src/test/java/de/thjom/java/systemd/types/UnitFileTypeTest.java
@@ -0,0 +1,57 @@
+/*
+ * Java-systemd implementation
+ * Copyright (c) 2016 Markus Enax
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of either the GNU Lesser General Public License Version 2 or the
+ * Academic Free Licence Version 3.0.
+ *
+ * Full licence texts are included in the COPYING file with this program.
+ */
+
+package de.thjom.java.systemd.types;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class UnitFileTypeTest {
+
+    @Test(description="Tests the constructor and getters.")
+    public void testStructure() {
+        UnitFileType type = new UnitFileType("/usr/lib/systemd/system/avahi-daemon.service", "loaded");
+
+        Assert.assertEquals(type.getPath(), "/usr/lib/systemd/system/avahi-daemon.service");
+        Assert.assertEquals(type.getStatus(), "loaded");
+    }
+
+    @Test(description="Tests the summary string aggregator.")
+    public void testSummary() {
+        UnitFileType type = new UnitFileType("/usr/lib/systemd/system/avahi-daemon.service", "loaded");
+
+        Assert.assertEquals(type.getSummary(), "/usr/lib/systemd/system/avahi-daemon.service loaded");
+    }
+
+    @Test(description="Tests the implemention of the 'Comparable' interface.")
+    public void testComparability() {
+        UnitFileType type1 = new UnitFileType("/usr/lib/systemd/system/avahi-daemon.service", "loaded");
+        UnitFileType type2 = new UnitFileType("/usr/lib/systemd/system/polkit.service", "loaded");
+
+        List<UnitFileType> list = new ArrayList<>();
+        list.add(type2);
+        list.add(type1);
+
+        Assert.assertSame(list.get(0), type2);
+        Assert.assertSame(list.get(1), type1);
+
+        Collections.sort(list);
+
+        Assert.assertSame(list.get(0), type1);
+        Assert.assertSame(list.get(1), type2);
+    }
+
+}
---
+++ src/test/java/de/thjom/java/systemd/types/UnitTypeTest.java
@@ -0,0 +1,94 @@
+/*
+ * Java-systemd implementation
+ * Copyright (c) 2016 Markus Enax
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of either the GNU Lesser General Public License Version 2 or the
+ * Academic Free Licence Version 3.0.
+ *
+ * Full licence texts are included in the COPYING file with this program.
+ */
+
+package de.thjom.java.systemd.types;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.freedesktop.dbus.Path;
+import org.freedesktop.dbus.UInt32;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import de.thjom.java.systemd.Systemd;
+import de.thjom.java.systemd.Unit;
+
+public class UnitTypeTest {
+
+    @Test(description="Tests the constructor and getters.")
+    public void testStructure() {
+        String unitName = "dummy.service";
+
+        UnitType type = new UnitType(unitName, "Dummy unit",
+                                     "loaded", "active", "running",
+                                     "Follow",
+                                     new Path(Unit.OBJECT_PATH + Systemd.escapePath(unitName)),
+                                     new UInt32(42L), "dummy type", new Path("dummy_path"));
+
+        Assert.assertEquals(type.getUnitName(), unitName);
+        Assert.assertEquals(type.getUnitDescription(), "Dummy unit");
+        Assert.assertEquals(type.getLoadState(), "loaded");
+        Assert.assertEquals(type.getActiveState(), "active");
+        Assert.assertEquals(type.getSubState(), "running");
+        Assert.assertEquals(type.getFollowingUnit(), "Follow");
+        Assert.assertEquals(type.getUnitObjectPath().getPath(), Unit.OBJECT_PATH + Systemd.escapePath(unitName));
+        Assert.assertEquals(type.getJobId(), 42);
+        Assert.assertEquals(type.getJobType(), "dummy type");
+        Assert.assertEquals(type.getJobObjectPath().getPath(), "dummy_path");
+    }
+
+    @Test(description="Tests the summary string aggregator.")
+    public void testSummary() {
+        String unitName = "dummy.service";
+
+        UnitType type = new UnitType(unitName, "Dummy unit",
+                "loaded", "active", "running",
+                "Follow",
+                new Path(Unit.OBJECT_PATH + Systemd.escapePath(unitName)),
+                new UInt32(42L), "dummy type", new Path("dummy_path"));
+
+        Assert.assertEquals(type.getSummary().length(), 239);
+    }
+
+    @Test(description="Tests the implemention of the 'Comparable' interface.")
+    public void testComparability() {
+        String unitName1 = "dummy1.service";
+
+        UnitType type1 = new UnitType(unitName1, "Dummy unit",
+                "loaded", "active", "running",
+                "Follow",
+                new Path(Unit.OBJECT_PATH + Systemd.escapePath(unitName1)),
+                new UInt32(42L), "dummy type", new Path("dummy_path"));
+
+        String unitName2 = "dummy2.service";
+
+        UnitType type2 = new UnitType(unitName2, "Dummy unit",
+                "loaded", "active", "running",
+                "Follow",
+                new Path(Unit.OBJECT_PATH + Systemd.escapePath(unitName2)),
+                new UInt32(42L), "dummy type", new Path("dummy_path"));
+
+        List<UnitType> list = new ArrayList<>();
+        list.add(type2);
+        list.add(type1);
+
+        Assert.assertSame(list.get(0), type2);
+        Assert.assertSame(list.get(1), type1);
+
+        Collections.sort(list);
+
+        Assert.assertSame(list.get(0), type1);
+        Assert.assertSame(list.get(1), type2);
+    }
+
+}
