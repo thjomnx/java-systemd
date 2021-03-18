94ab024cb98a875a2726f98dee8ffb9c7baa8236
Markus Enax <Markus.Enax@gmail.com>
2021-03-18 21:50:25 +0100

    Temporarily fix testing error due to modularization
    dbus-java uses reflective operation `Field.setAccessible()` for internal
    parameter setup (triggered by `equals()` and `toString()`) which causes
    unit test errors.
    
    This is a temporary fix. It is currently unclear whether JVM runtimes of
    modularized projects are also affected.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module de.thjom.java.systemd {
 	requires transitive org.freedesktop.dbus;
 	requires transitive java.xml.bind;
+	exports de.thjom.java.systemd;
 	exports de.thjom.java.systemd.features;
 	exports de.thjom.java.systemd.interfaces;
 	exports de.thjom.java.systemd.types;
-	exports de.thjom.java.systemd;
 }
